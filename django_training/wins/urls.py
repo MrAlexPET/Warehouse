680540c9380fa17e8244dc697a3e4d1735035110
Alex Petrusevich <apetrusevic046@gmail.com>
2023-09-21 02:15:51 +0300

    added first versions of wins/male and wins/female pages

--- django_training/wins/urls.py
+++ django_training/wins/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.wins_main, name='wins_main')
+    path('', views.wins_main, name='wins_main'),
+    path('male/', views.wins_male, name='wins_male'),
+    path('female/', views.wins_female, name='wins_female')
 ]
--- django_training/wins/views.py
+++ django_training/wins/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def wins_main(request):
     return render(request, 'wins/wins.html')
+
+
+def wins_male(request):
+    return render(request, 'wins/male.html')
+
+
+def wins_female(request):
+    return render(request, 'wins/female.html')
