2dfbbf05548072d56cea8b6941a0463861c55591
Alex Petrusevich <apetrusevic046@gmail.com>
2023-09-21 03:33:08 +0300

    changed settings.py (using mysql instead sqlite)

--- django_training/django_training/settings.py
+++ django_training/django_training/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_training.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kpop_warehouse',  # Имя вашей базы данных
+        'USER': 'root',  # Имя пользователя базы данных
+        'PASSWORD': 'alex11',  # Пароль пользователя базы данных
+        'HOST': 'localhost',  # Адрес сервера MySQL (обычно localhost)
+        'PORT': '3306',
     }
 }
 
